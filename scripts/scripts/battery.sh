84ee2f87200b480107f0a8770bf4724e5ba0d953
Elizarov Dmitry <d.elizarov.00@gmail.com>
2023-01-17 15:21:24 +0300

    bash -> sh, changed scripts in dir

--- scripts/scripts/battery.sh
+++ scripts/scripts/battery.sh
@@ -1,7 +1,21 @@
 #!/usr/bin/sh
 
-cap="$(cat /sys/class/power_supply/BAT0/capacity)"
-cap_level="$(cat /sys/class/power_supply/BAT0/capacity_level)"
+bat_file="/sys/class/power_supply/BAT0/capacity"
+
+# cap="$(cat /sys/class/power_supply/BAT0/capacity)"
+# cap_level="$(cat /sys/class/power_supply/BAT0/capacity_level)"
+
+if [ -f bat_file ]
+then
+    echo "file exists"
+else
+    echo ""
+fi
 
 
-echo "$cap% $cap_level"
+# [ -f bat_file ] && {echo "bat_file exists"} || { echo "bat_file doesn't exist"}
+
+# [ -z cap ] && echo "" || {
+#         echo "NO BAT"
+#     }
+# echo "$cap% $cap_level"
